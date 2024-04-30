b866ad79737f5b27394ff21fc44798947c04db76
masal <marcal.herraiz.7e5@itb.cat>
2024-04-30 16:09:47 +0200

    actualizacion resutltorow profesor

--- src/main/kotlin/com/example/dao/usuarios/profesor/DAOProfesorImpl.kt
+++ src/main/kotlin/com/example/dao/usuarios/profesor/DAOProfesorImpl.kt
@@ -1,8 +1,7 @@
 package com.example.dao.usuarios.profesor
 
 import com.example.dao.DataBaseConnection
-import com.example.dao.usuarios.alumno.DAOAlumno
-import com.example.dao.usuarios.alumno.DAOAlumnoImpl
+
 import com.example.model.usuarios.Profesor
 import com.example.model.usuarios.Profesores
 import org.jetbrains.exposed.sql.ResultRow
@@ -12,11 +11,12 @@ class DAOProfesorImpl: DAOProfesor {
     private fun resultToRowProfesor(row: ResultRow) = Profesor(
         idPorfesor = row[Profesores.idProfesor],
         nombre = row[Profesores.nombre],
-        apellido1 = row[Profesores.apellido1],
-        apellido2 = row[Profesores.apellido2],
-        dni = row[Profesores.dni],
+        apellidos = row[Profesores.apellidos],
         correo = row[Profesores.correo],
         identificador = row[Profesores.identificador],
+        etiqueta = row[Profesores.etiqueta],
+        categoria = row[Profesores.categoria],
+        grupos = row[Profesores.grupos],
         contrasenya = row[Profesores.contrasenya],
         tutor = row[Profesores.tutor],
         admin = row[Profesores.admin]
