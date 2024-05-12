93b8e63768d7c95fb7b5ef3afcc0b84a4b4d3bcd
Your Name <you@example.com>
2024-05-12 21:11:49 +0200

    retocar modulo

--- src/main/kotlin/com/example/model/Modulo.kt
+++ src/main/kotlin/com/example/model/Modulo.kt
@@ -21,7 +21,7 @@ object Modulos: Table("modulo"){
     val codCiclo = varchar("codciclo", 55)
     val codCompleto = varchar("codcompleto", 55)
     val nombreCiclo = varchar("nombreciclo", 255)
-    val turno = varchar("nombreciclo", 55)
+    val turno = varchar("turno", 55)
     val grupo = varchar("grupo", 55)
     val numModulo = varchar("nummodulo", 55)
     val nombreModulo = varchar("nombremodulo", 255)
